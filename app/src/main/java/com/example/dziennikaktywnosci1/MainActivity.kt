3cf4d9c6239e9f9b9900e1e46b2136d133c94cf7
podkalinho95 <tomek.pokalickI@o2.pl>
2024-09-12 21:36:04 +0200

    bugged up 6

--- app/src/main/java/com/example/dziennikaktywnosci1/MainActivity.kt
+++ app/src/main/java/com/example/dziennikaktywnosci1/MainActivity.kt
@@ -2,14 +2,19 @@ package com.example.dziennikaktywnosci1
 
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.example.dziennikaktywnosci1.data.models.Transaction
+import com.example.dziennikaktywnosci1.data.models.TransactionCategory
+import com.example.dziennikaktywnosci1.data.models.TransactionType
 import com.example.dziennikaktywnosci1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val mainVm by viewModels<MainViewModel>()
     private lateinit var binding: ActivityMainBinding
         private lateinit var navController: NavController
 
@@ -21,7 +26,10 @@ class MainActivity : AppCompatActivity() {
             val navHostFragment = supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
             navController = navHostFragment.navController
             NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+            mainVm.insertTransaction(createTransaction())
 
 
         }
+    private fun createTransaction() = Transaction(0,1L,
+        15, "opis", TransactionType.INCOME, TransactionCategory.TRANSPORTATION)
     }
--- app/src/main/java/com/example/dziennikaktywnosci1/ui/transactions_fragment/TransactionsFragment.kt
+++ app/src/main/java/com/example/dziennikaktywnosci1/ui/transactions_fragment/TransactionsFragment.kt
@@ -2,26 +2,45 @@ package com.example.dziennikaktywnosci1.ui.transactions_fragment
 
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.dziennikaktywnosci1.MainViewModel
 import com.example.dziennikaktywnosci1.R
+import com.example.dziennikaktywnosci1.databinding.FragmentTransactionsBinding
+import com.example.dziennikaktywnosci1.ui.adapters.TransactionAdapter
 
 class TransactionsFragment : Fragment() {
 
     private val viewModel by viewModels<TransactionsViewModel>()
+    private val mainVm by activityViewModels<MainViewModel>()
+    private var _binding : FragmentTransactionsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_transactions, container, false)
+        _binding = FragmentTransactionsBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View,
                                savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        mainVm.getAllTransaction().observe(viewLifecycleOwner) { transactions ->
+            binding.recyclerView.adapter = TransactionAdapter(
+                transactions,
+                { transaction, position ->
+                    Log.d("Test", "Transaction: ${transaction.toString()}")
+                }
+            )
+        }
 
     }
 }
